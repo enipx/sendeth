acf056098178a3c8753c3918fcc57e603b52a8b2
enipx <progenipx@gmail.com>
2023-05-06 05:22:13 +0100

    chore: improvements

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,17 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { OreoProvider } from '@oreo-ui/web'
+import { OreoProvider, DefaultTheme } from '@oreo-ui/web'
 import { TransactionProvider } from '@/context/TransactionContext'
-
-const theme = {
-  fonts: {
-    body: 'Inter'
-  }
-}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <OreoProvider theme={theme as any}>
+    <OreoProvider>
       <TransactionProvider>
         <Component {...pageProps} />
       </TransactionProvider>
