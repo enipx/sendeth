e4de276ed40f16e80c8c94f90a972dc4d70002a0
enipx <progenipx@gmail.com>
2023-05-06 05:46:59 +0100

    feat: add dark mode support

--- client/src/component/footer.tsx
+++ client/src/component/footer.tsx
@@ -1,11 +1,33 @@
-import { View, Text } from "@oreo-ui/web"
+import { View, Text, Switch, useModeContext, useModeTheme, Flex } from "@oreo-ui/web"
+import { ChangeEvent } from "react";
 
 const Footer = () => {
+  const mode = useModeContext();
+
+  const { isDark } = useModeTheme();
+
+  const onChangeHandler = (evt: ChangeEvent<HTMLInputElement>) => {
+    evt.preventDefault();
+    mode?.toggle();
+  }
+
   return (
     <View py="md">
       <Text opacity="0.85" fontSize="sm" textAlign="center">
         Copyright ©2023. All rights reserved.
       </Text>
+
+      <Flex center mt="2xl" column>
+        <Switch
+          checked={isDark}
+          onChange={onChangeHandler}
+          key={isDark as any}
+        />
+
+        <Text mt="md" fontSize="sm" fontWeight="500" textAlign="center">
+          Switch to {isDark ? 'light' : 'dark'} mode
+        </Text>
+      </Flex>
     </View>
   )
 }
